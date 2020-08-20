667e8de7df1d3d31aa3fe0f33f497c8923c94979
twzkraus <twzkraus@gmail.com>
2020-08-19 21:08:47 -0700

    implemented es6 stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+  }
+
+  push(value) {
+    this.length++;
+    this[this.length] = value;
+  }
+
+  pop() {
+    var lastElement = this[this.length];
+    delete this[this.length];
+    if (this.length !== 0) {
+      this.length--;
+    }
+    return lastElement;
+  }
+
+  size() {
+    return this.length;
   }
 
 }
