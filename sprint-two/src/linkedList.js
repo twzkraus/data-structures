585bd7f965be139eb3da99fb95da524b37cf6e21
twzkraus <twzkraus@gmail.com>
2020-08-20 17:36:35 -0700

    partial implementation of LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,34 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  // let numOfNodes = 0;
 
   list.addToTail = function(value) {
+    // let key = numOfNodes++;
+    let newNode = Node(value);
+    if (!list.head) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
+  /*
+  linkedList.addToTail(4);                    (4) -> null
+  linkedList.addToTail(5);                    (4) -> (5) -> null
+  linkedList.head.next =
+  expect(linkedList.head.value).to.equal(4);
+  linkedList.removeHead();
+  expect(linkedList.head.value).to.equal(5);
+  */
+
   list.removeHead = function() {
+    let oldHead = list.head;
+    list.head = list.head.next;
+    delete list.head;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
