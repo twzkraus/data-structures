a48bd76a710349786d8ee536d9163af2e380c948
twzkraus <twzkraus@gmail.com>
2020-08-21 21:20:49 -0700

    partially implemented collision handler in hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,37 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let tuple = [k, v];
+  // if our storage spot is empty,
+  if (!this._storage.get(index).length) {
+    // set the storage spot equal to our tuple
+    this._storage.set(index, [tuple]);
+  } else {
+  // otherwise,
+    // get the current storage spot
+    var currentStorage = this._storage.get(index);
+    // push the new tuple into the storage spot value
+    currentStorage.push(tuple);
+    // set the storage spot equal to an array with the new tuple concatenated to the end of previous tuples
+    this._storage.set(index, currentStorage);
+  }
 };
+// previously:
+// storageArray[index]: ['turner', 'kraus']
+// now:
+// storageArray[index]: [['turner', 'kraus']]
+// or: [['turner', 'kraus'], ['scott', 'mounce']]
+
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[1];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  let tuple = [];
+  this._storage.set(index, tuple);
 };
 
 
